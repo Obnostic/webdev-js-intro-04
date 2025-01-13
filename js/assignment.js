00275a38374e65236dd638d26bbdd942ce4915d9
Steven <unklfoddy@gmail.com>
2025-01-13 15:26:57 -0500

    incomplete

--- js/assignment.js
+++ js/assignment.js
@@ -3,28 +3,34 @@
 // Create your references to the html elements here
 const ageInputEl = document.getElementById("age-input")
 const submissionBtn = document.getElementById("submission-btn");
-
-// create a mutable variable called age and do not assign it a value.
+const response = document.getElementById("p");
+let age;
 
 
 function checkAgeAndRespond() {
+
     age = parseInt(ageInputEl.value);
     
-    // Write your code below
+    if (age >= 21) {
+        document.write("You can vote and purchase alcohol.");  
+    } else if (age >=18) {
+        document.write("You can vote and purchase alcohol.");  
+    } else {
+        document.write("You cannot vote and you cannot purchase alcohol.");  
+    }
+};
 
-}
 
 
-submissionBtn.addEventListener("click", function () {
-    checkAgeAndRespond();
-});
-
+submissionBtn.addEventListener("click", function)
+    {  checkAgeAndRespond(); };
+   
 
 function validateInput(event) {
     const invalidCharacters = ["e", "E", "-", "+"];
     if (invalidCharacters.includes(event.key)) {
         event.preventDefault();
     }
-}
+};
 
 ageInputEl.addEventListener("keydown", validateInput);
--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/* let healthPoints = -5;
+
+if (healthPoints >= 10) {
+    console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+    console.log("Hero is slightly damaged.");
+} else if (healthPoints < 5 && healthPoints >0) {
+    console.log("Hero is close to death.");
+} else {
+    console.log("Sorry bud, your hero's dead, nice going Dr. Slugspeed!");
+}
+    */
